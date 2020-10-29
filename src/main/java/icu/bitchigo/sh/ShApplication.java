d02793bccccf03336b929f3d41ea1d2a8a95cccb
bitchigo <1020142782@qq.com>
2020-10-29 14:37:37 +0800

    nose

--- src/main/java/icu/bitchigo/sh/ShApplication.java
+++ src/main/java/icu/bitchigo/sh/ShApplication.java
@@ -3,13 +3,11 @@ package icu.bitchigo.sh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan("icu.bitchigo.sh.mapper")
-@EnableWebSecurity
 public class ShApplication {
 
     public static void main(String[] args) {
